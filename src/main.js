2a598814c78f8233d6426cd10e81cd20134a6b2a
dj8389 <88929687+dj8389@users.noreply.github.com>
2021-11-25 16:09:40 +0800

    version-1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+//import { createApp } from 'vue'
+//import App from './App.vue'
 
-createApp(App).mount('#app')
+//createApp(App).mount('#app')
+
+const app = Vue.createApp({
+    data() {
+        return {
+            tasks: [{
+                name: 'Task1',
+                description: '...',
+                endDate: 'tmrw',
+                complete: true
+            },
+            {
+                name: 'Task2',
+                description: '1234',
+                endDate: '..',
+                complete: false
+
+            }]
+        }
+    },
+    methods: {
+        addTask(task) {
+            this.tasks.push(task)
+        }
+    }
+})
