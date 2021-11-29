29ed01793af81964b018a620d162cf55dbbd6dfe
dj8389 <88929687+dj8389@users.noreply.github.com>
2021-11-29 15:46:29 +0800

    using single file components

---
+++ src/components/TaskList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="task-list">
+    <table>
+      <tr>
+        <th id="name-header">Name</th>
+        <th id="description-header">Description</th>
+        <th id="endDate-header">End Date</th>
+        <th>Complete</th>
+      </tr>
+      <tr v-for="(task, index) in tasks" :key="index">
+        <td id="name-header">{{ task.name }}</td>
+        <td id="description-header">{{ task.description }}</td>
+        <td id="endDate-header">{{ task.endDate }}</td>
+        <td>
+          <input type="checkbox" />
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: Array,
+  },
+};
+</script>
+
+<style>
+</style>
